e55df5d170a3751ef71da64792f420a6fff73066
Mani Movassagh <mani.mgh@gmail.com>
2025-09-08 21:55:34 +0200

    Add font validation functions to check for pretty keywords and length

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"strings"
+)
+
+// Checks if the font name contains a pretty keyword
+func IsPrettyFont(font string) bool {
+	keywords := []string{"Sans", "Serif", "Mono"}
+	for _, kw := range keywords {
+		if strings.Contains(strings.ToLower(font), strings.ToLower(kw)) {
+			return true
+		}
+	}
+	return false
+}
+
+// Checks if the font name is long enough (at least 5 characters)
+func IsFontNameLongEnough(font string) bool {
+	return len(font) >= 5
+}
+
+// Combined check: pretty and long enough
+func IsFontPrettyEnough(font string) bool {
+	return IsPrettyFont(font) && IsFontNameLongEnough(font)
+}
